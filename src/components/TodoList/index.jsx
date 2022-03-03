2de13e036590186664649c8a272c9d1efbd3d0df
rroderickk <rroderickk@gmail.com>
2022-03-02 21:12:33 -0500

    [ADD] version 0.0.5

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -7,7 +7,7 @@ const TodoList =(props)=> { return ( <>
 <section>
 <div className="todoList-box"><h4 className="todoList-banner">{c}</h4></div>
   <ul className="todoList-ul">{props.children}</ul>
-<div className="todoList-cheatmodes"><p>{c}{<br />}{a}{<br/>}{c}</p>{"demo: version 0.0.4"}</div>
+<div className="todoList-cheatmodes"><p>{c}{<br />}{a}{<br/>}{c}</p>{"demo: version 0.0.5"}</div>
 </section>
 
 </> ) }; export { TodoList };
