8a7805187ccb50155a8c28e593db405c946f47c0
rroderickk <rroderickk@gmail.com>
2022-03-04 08:05:58 -0500

    [ADD] zoom effect

--- src/components/Modal/Index.jsx
+++ src/components/Modal/Index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./Modal.scss"; //!(:@)====>styles;
+import "./Modal.scss";//!(:@)====>styles;
 
 const Modal = ({ children }) => {
 
